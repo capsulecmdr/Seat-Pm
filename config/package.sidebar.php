d1442e6908895ac92f7d6615a854ef52c1665190
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-04-24 06:37:52 -0700

    Create package.sidebar.php

---
+++ config/package.sidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'seatpm' => [
+        'name' => 'SeAT-PM',
+        'icon' => 'fas fa-project-diagram',
+        'route' => 'seatpm.projects.index',
+        'permission' => 'seatpm.projects.create', // or leave null for all users
+    ],
+];
