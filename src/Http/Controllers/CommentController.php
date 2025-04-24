7683c18578dafb3f52cde9d9816022ba3590139d
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-04-24 06:29:39 -0700

    Update CommentController.php

--- src/Http/Controllers/CommentController.php
+++ src/Http/Controllers/CommentController.php
@@ -7,6 +7,7 @@ use CapsuleCmdr\SeATPM\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Routing\Controller;
 
 class CommentController extends Controller
 {
