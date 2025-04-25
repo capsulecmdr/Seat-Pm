62f02123678491db4f7b58033f8c0d29299d13be
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-04-25 09:36:47 -0700

    controller update

--- src/Http/Controllers/ProjectController.php
+++ src/Http/Controllers/ProjectController.php
@@ -6,9 +6,11 @@ use CapsuleCmdr\SeATPM\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ProjectController extends Controller
 {
+    use AuthorizesRequests;
     /**
      * Display a list of projects filtered by visibility scope.
      */
