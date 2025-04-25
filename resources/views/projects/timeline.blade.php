c6789dd1598fda6e55a327e71dbdc454c11a3d74
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-04-25 14:48:04 -0700

    timeline blade update

--- resources/views/projects/timeline.blade.php
+++ resources/views/projects/timeline.blade.php
@@ -1,5 +1,5 @@
 <div class="list-group">
-    @forelse ($project->comments as $comment)
+    @forelse ($project->comments ?? [] as $comment)
         <div class="list-group-item">
             <small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small>
             <p>{{ $comment->body }}</p>
